2cc03d1cee0b761fb7ee902b210c7c3a5ba190a4
zhiqiang <wzqhbkjdx@126.com>
2018-07-09 20:01:59 +0800

    add webview

---
+++ app/src/main/java/cent/news/com/newscent/view/WebViewActivity.java
@@ -0,0 +1,20 @@
+package cent.news.com.newscent.view;
+
+import android.os.Bundle;
+
+import cent.news.com.baseframe.view.BaseActivity;
+import cent.news.com.baseframe.view.BaseBuilder;
+
+public class WebViewActivity extends BaseActivity<WebViewBiz> {
+
+    @Override
+    protected BaseBuilder build(BaseBuilder builder) {
+        return null;
+    }
+
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
+    }
+
+}
---
+++ app/src/main/java/cent/news/com/newscent/view/WebViewBiz.java
@@ -0,0 +1,10 @@
+package cent.news.com.newscent.view;
+
+import cent.news.com.baseframe.core.BaseBiz;
+
+public class WebViewBiz extends BaseBiz<WebViewActivity> {
+
+
+
+
+}
