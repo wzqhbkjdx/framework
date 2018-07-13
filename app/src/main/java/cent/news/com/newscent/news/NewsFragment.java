b46febcd648a6111b70ccb16f37d977825884514
zhiqiang <wzqhbkjdx@126.com>
2018-07-13 19:31:10 +0800

    edit news fragment

--- app/src/main/java/cent/news/com/newscent/news/NewsFragment.java
+++ app/src/main/java/cent/news/com/newscent/news/NewsFragment.java
@@ -1,22 +1,47 @@
 package cent.news.com.newscent.news;
 
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 
 import cent.news.com.baseframe.view.BaseBuilder;
 import cent.news.com.baseframe.view.BaseFragment;
+import cent.news.com.newscent.R;
 
-public class NewsFragment extends BaseFragment<NewsBiz> {
-
+public class NewsFragment extends BaseFragment<NewsBiz> implements ViewPager.OnPageChangeListener {
 
 
     @Override
     protected BaseBuilder build(BaseBuilder builder) {
+        builder.layoutId(R.layout.fragment_news_layout);
+        builder.tintFitsSystem(false);
+        return builder;
+    }
 
-        return null;
+    @Override
+    protected void initData(Bundle savedInstanceState) {
+
     }
 
     @Override
     protected void buildAfter(View view) {
 
     }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+
 }
